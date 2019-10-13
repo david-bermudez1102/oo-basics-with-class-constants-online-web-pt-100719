557bcb69e53aaab0c0177a4bca046da0bdd905ad
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-13 17:48:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if(!BRANDS.contais)
   end
 
   def cobble
@@ -14,4 +14,7 @@ class Shoe
   end
 
   def brand
+
+  end
+
 end
