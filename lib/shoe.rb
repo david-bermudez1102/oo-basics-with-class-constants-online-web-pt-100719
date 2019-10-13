e0143a3bd91243a80e5d7062ae20e478565dd5d7
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-13 17:46:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand, :brands
+  attr_reader :brand
 
   BRANDS = []
 
@@ -13,4 +13,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand
 end
